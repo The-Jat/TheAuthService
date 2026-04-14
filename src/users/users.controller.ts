1b2818d1da1ab9f4e01ff916387b07b9fe5c0c57
TheJat <manishkl543@gmail.com>
2026-04-14 21:09:33 +0530

    create role based guard RBAC for the users endpoint

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,8 @@
 import { Body, Controller, Get, Post, Query, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { JwtAuthGuard } from '../auth/jwt.guard';
+import { RolesGuard } from '../auth/roles.guard';
+import { Roles } from '../auth/roles.decorator';
 
 @Controller('users')
 export class UsersController {
@@ -13,7 +15,8 @@ export class UsersController {
   }
 
   @Get()
-  @UseGuards(JwtAuthGuard)
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('admin')
   getUser(@Query('email') email: string) {
     return this.usersService.findByEmail(email);
   }
